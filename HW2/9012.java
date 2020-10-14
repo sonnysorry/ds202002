18f6f5de12fc85b786d4332c6b29392a3ab27405
Hyeong-do Son <61896873+sonnysorry@users.noreply.github.com>
2020-10-15 01:06:26 +0900

    Update 9012.java
    수정되지 않은 파일이 올라가 수정했습니다.

--- HW2/9012.java
+++ HW2/9012.java
@@ -1,10 +1,37 @@
 package hw2;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int repeat = Integer.parseInt(scan.next());
+		for (int j = 0; j < repeat; j++) {
 
+			String input = scan.next();
+			String res = "YES";
+			Stack st = new Stack<>();
+			for (int i = 0; i < input.length(); i++) {
+				if (input.charAt(i) == '(') {
+					st.push(1);
+				} else if (input.charAt(i) == ')') {
+					if (st.isEmpty()) {
+						res = "NO";
+						break;
+					} else {
+						st.pop();
+					}
+				}
+		}
+		if(!st.isEmpty()) {
+			res = "NO";
+		}
+		System.out.println(res);
+		}
 	}
 
 }
+
